d89796b8b91d02c41ed9389d776747bf0c021b14
EroMCakes <salah17afifi@gmail.com>
2024-08-29 11:50:47 +0100

    trilingual project iwht tgz ignoring

---
+++ TMHKTM v3.1.3f2_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,185 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void cobraController::Start()
+extern void cobraController_Start_m4E634AF5B5746D3715A665E1E92D59252507A5FA (void);
+// 0x00000002 System.Void cobraController::EnterLetterAs(System.String)
+extern void cobraController_EnterLetterAs_m8CDEB215E167FC3F2B27699FFCAB080BEA48293C (void);
+// 0x00000003 System.Void cobraController::BackSpace()
+extern void cobraController_BackSpace_m8B4D606B268EFC96531ABF3564A7A8562939D984 (void);
+// 0x00000004 System.Void cobraController::ResetStringCode()
+extern void cobraController_ResetStringCode_m728701D209210EF9C513D59847964E9659647659 (void);
+// 0x00000005 System.Void cobraController::CodeValidation()
+extern void cobraController_CodeValidation_m171BFCF65C2C424B8676CD0463865D4A2DEF8560 (void);
+// 0x00000006 System.Void cobraController::.ctor()
+extern void cobraController__ctor_m367FD99FE4685BCECB60819C44DD5E89C7A12093 (void);
+// 0x00000007 System.Void ColorPanelController::EnterDigitNumber(System.Int32)
+extern void ColorPanelController_EnterDigitNumber_m6BAC753E8D3B629DF903D258DF07678CEB8CB35D (void);
+// 0x00000008 System.Void ColorPanelController::BackSpace()
+extern void ColorPanelController_BackSpace_m5A14997739CCFF2853B45080172CFA2B9756B2AE (void);
+// 0x00000009 System.Void ColorPanelController::ResetCode()
+extern void ColorPanelController_ResetCode_m132B86B9BB1D1DB1E94026C9BC63795609843808 (void);
+// 0x0000000A System.Void ColorPanelController::BackButton()
+extern void ColorPanelController_BackButton_m09FFD540C4DCCC2B17A9FFE715E0ED8B0672658E (void);
+// 0x0000000B System.Void ColorPanelController::ValidateColorCode()
+extern void ColorPanelController_ValidateColorCode_m14691D365E012E4269A97BC9552A008FF2E2F995 (void);
+// 0x0000000C System.Void ColorPanelController::.ctor()
+extern void ColorPanelController__ctor_m12832272B46BEC246F3CF54FD518D08D7B69BAB8 (void);
+// 0x0000000D System.Void Image_Switch_Eyes::Switch_Image()
+extern void Image_Switch_Eyes_Switch_Image_m4CEBD690290D20DF4E522D87BBA27CFC9761B175 (void);
+// 0x0000000E System.Void Image_Switch_Eyes::.ctor()
+extern void Image_Switch_Eyes__ctor_m35BAE2B8A7E8F7F077909E4B103E677EDCF524AA (void);
+// 0x0000000F System.Void Image_Switch_Head::Switch_Image()
+extern void Image_Switch_Head_Switch_Image_mDEEEA39EC2E6CF28F1A55D9AC64ABA710FA802AF (void);
+// 0x00000010 System.Void Image_Switch_Head::.ctor()
+extern void Image_Switch_Head__ctor_mF83ECF39818D117C54AB4E6043F5B794CF2C460C (void);
+// 0x00000011 System.Void Image_Switch_Mouth::Switch_Image()
+extern void Image_Switch_Mouth_Switch_Image_m43CF47C1CC2AD6369EB8CBA47D5CE1FE5CB176F6 (void);
+// 0x00000012 System.Void Image_Switch_Mouth::.ctor()
+extern void Image_Switch_Mouth__ctor_m62DA544C223FE2FDD507D92B4B08633314C9B735 (void);
+// 0x00000013 System.Void MonumentController::Start()
+extern void MonumentController_Start_m3FB04898277A95E9D83B4AD11B714EC5B1C47EA3 (void);
+// 0x00000014 System.Void MonumentController::.ctor()
+extern void MonumentController__ctor_m2E2AC904EC7CD648ACD36779BE1C0741DA1E8440 (void);
+// 0x00000015 System.Void MuskPanelController::ResetCodeFields()
+extern void MuskPanelController_ResetCodeFields_m7DCCE89CA6F5B246A7DA6DEE3308157F3338A1DD (void);
+// 0x00000016 System.Void MuskPanelController::ValidateCodeFields()
+extern void MuskPanelController_ValidateCodeFields_m20AAC8CC85731D8CF0C34012EA84D54E2A4274A2 (void);
+// 0x00000017 System.Void MuskPanelController::LoadBackView()
+extern void MuskPanelController_LoadBackView_mAB6D0B2E4B5FD3CFD9E09A58C6AA82DDF9566044 (void);
+// 0x00000018 System.Void MuskPanelController::Start()
+extern void MuskPanelController_Start_mAA0F7E1A87D89FB9E39659858E9BE7C3E360CF17 (void);
+// 0x00000019 System.Void MuskPanelController::.ctor()
+extern void MuskPanelController__ctor_m805967686F6A9D227F767C2FC03D02FFE2F4EFCA (void);
+// 0x0000001A System.Void PanelController::Awake()
+extern void PanelController_Awake_mF7CE88F6F87FC58A55C87A023601A802B00CB37E (void);
+// 0x0000001B System.Void PanelController::.ctor()
+extern void PanelController__ctor_mD7FA64FF1D8A83D3DDB3E4FE88C75DE84257E58A (void);
+// 0x0000001C System.Void PortraitPadManager::EnterDigit(System.Int32)
+extern void PortraitPadManager_EnterDigit_m6A809598BC8870848124DDC986471A30B4A77BDE (void);
+// 0x0000001D System.Void PortraitPadManager::ResetCodeField()
+extern void PortraitPadManager_ResetCodeField_mF906EB0C5311C14076E4B5D74313564F44E92FA1 (void);
+// 0x0000001E System.Void PortraitPadManager::ValidateCodeField()
+extern void PortraitPadManager_ValidateCodeField_m21E0D16635F5BFE53384138CBFFF2B5E4C5903D4 (void);
+// 0x0000001F System.Void PortraitPadManager::BackButton()
+extern void PortraitPadManager_BackButton_m9C6FD6A4429C26F18CDA1F53C09F1B0BB983B321 (void);
+// 0x00000020 System.Void PortraitPadManager::.ctor()
+extern void PortraitPadManager__ctor_m95964C60F8576C83F4564E0C0CD319583957052C (void);
+// 0x00000021 System.Void ViewControllTools.ViewController::LoadViewWithIndex(System.Int32)
+extern void ViewController_LoadViewWithIndex_m13EDC024865B2D53F6CD8C4DD36BA5F40A676EF9 (void);
+// 0x00000022 System.Void ViewControllTools.ViewController::DestroyCurrentViewPanel()
+extern void ViewController_DestroyCurrentViewPanel_mFBB6F3909B02B6DC67976743432AAC12075BF123 (void);
+// 0x00000023 System.Void ViewControllTools.ViewController::SelectLanguage(System.String)
+extern void ViewController_SelectLanguage_m70BA7ECD48BDCC2ACDC985D31CB0374326D600D9 (void);
+// 0x00000024 System.Void ViewControllTools.ViewController::.ctor()
+extern void ViewController__ctor_m7FE4A755C5F1AF7C487CB655FE7A0938F3054B77 (void);
+static Il2CppMethodPointer s_methodPointers[36] = 
+{
+	cobraController_Start_m4E634AF5B5746D3715A665E1E92D59252507A5FA,
+	cobraController_EnterLetterAs_m8CDEB215E167FC3F2B27699FFCAB080BEA48293C,
+	cobraController_BackSpace_m8B4D606B268EFC96531ABF3564A7A8562939D984,
+	cobraController_ResetStringCode_m728701D209210EF9C513D59847964E9659647659,
+	cobraController_CodeValidation_m171BFCF65C2C424B8676CD0463865D4A2DEF8560,
+	cobraController__ctor_m367FD99FE4685BCECB60819C44DD5E89C7A12093,
+	ColorPanelController_EnterDigitNumber_m6BAC753E8D3B629DF903D258DF07678CEB8CB35D,
+	ColorPanelController_BackSpace_m5A14997739CCFF2853B45080172CFA2B9756B2AE,
+	ColorPanelController_ResetCode_m132B86B9BB1D1DB1E94026C9BC63795609843808,
+	ColorPanelController_BackButton_m09FFD540C4DCCC2B17A9FFE715E0ED8B0672658E,
+	ColorPanelController_ValidateColorCode_m14691D365E012E4269A97BC9552A008FF2E2F995,
+	ColorPanelController__ctor_m12832272B46BEC246F3CF54FD518D08D7B69BAB8,
+	Image_Switch_Eyes_Switch_Image_m4CEBD690290D20DF4E522D87BBA27CFC9761B175,
+	Image_Switch_Eyes__ctor_m35BAE2B8A7E8F7F077909E4B103E677EDCF524AA,
+	Image_Switch_Head_Switch_Image_mDEEEA39EC2E6CF28F1A55D9AC64ABA710FA802AF,
+	Image_Switch_Head__ctor_mF83ECF39818D117C54AB4E6043F5B794CF2C460C,
+	Image_Switch_Mouth_Switch_Image_m43CF47C1CC2AD6369EB8CBA47D5CE1FE5CB176F6,
+	Image_Switch_Mouth__ctor_m62DA544C223FE2FDD507D92B4B08633314C9B735,
+	MonumentController_Start_m3FB04898277A95E9D83B4AD11B714EC5B1C47EA3,
+	MonumentController__ctor_m2E2AC904EC7CD648ACD36779BE1C0741DA1E8440,
+	MuskPanelController_ResetCodeFields_m7DCCE89CA6F5B246A7DA6DEE3308157F3338A1DD,
+	MuskPanelController_ValidateCodeFields_m20AAC8CC85731D8CF0C34012EA84D54E2A4274A2,
+	MuskPanelController_LoadBackView_mAB6D0B2E4B5FD3CFD9E09A58C6AA82DDF9566044,
+	MuskPanelController_Start_mAA0F7E1A87D89FB9E39659858E9BE7C3E360CF17,
+	MuskPanelController__ctor_m805967686F6A9D227F767C2FC03D02FFE2F4EFCA,
+	PanelController_Awake_mF7CE88F6F87FC58A55C87A023601A802B00CB37E,
+	PanelController__ctor_mD7FA64FF1D8A83D3DDB3E4FE88C75DE84257E58A,
+	PortraitPadManager_EnterDigit_m6A809598BC8870848124DDC986471A30B4A77BDE,
+	PortraitPadManager_ResetCodeField_mF906EB0C5311C14076E4B5D74313564F44E92FA1,
+	PortraitPadManager_ValidateCodeField_m21E0D16635F5BFE53384138CBFFF2B5E4C5903D4,
+	PortraitPadManager_BackButton_m9C6FD6A4429C26F18CDA1F53C09F1B0BB983B321,
+	PortraitPadManager__ctor_m95964C60F8576C83F4564E0C0CD319583957052C,
+	ViewController_LoadViewWithIndex_m13EDC024865B2D53F6CD8C4DD36BA5F40A676EF9,
+	ViewController_DestroyCurrentViewPanel_mFBB6F3909B02B6DC67976743432AAC12075BF123,
+	ViewController_SelectLanguage_m70BA7ECD48BDCC2ACDC985D31CB0374326D600D9,
+	ViewController__ctor_m7FE4A755C5F1AF7C487CB655FE7A0938F3054B77,
+};
+static const int32_t s_InvokerIndices[36] = 
+{
+	4875,
+	3868,
+	4875,
+	4875,
+	4875,
+	4875,
+	3848,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	4875,
+	3848,
+	4875,
+	4875,
+	4875,
+	4875,
+	7155,
+	7319,
+	3868,
+	4875,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	36,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
